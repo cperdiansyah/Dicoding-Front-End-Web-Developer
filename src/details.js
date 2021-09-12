40ca21a45a7526dd14d40dff8b0fabe4be05cf35
cperdiansyah <chandraperdiansyah@gmail.com>
2021-09-12 11:45:15 +0700

    add actor scroll

--- src/details.js
+++ src/details.js
@@ -2,13 +2,21 @@ import 'regenerator-runtime';
 import './style/style.css';
 import './style/detail.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
 
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import main from './scripts/pages/details/details';
+
+const tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
 
 document.addEventListener('DOMContentLoaded', main);
